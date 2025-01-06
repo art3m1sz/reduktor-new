b05076f57066c2f31b7ee7fe67bf3df25ad91473
art3m1sz <abhimantraputra@gmail.com>
2025-01-06 23:22:36 +0800

    add tech stack

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
 import { Avatar } from "@/components/ui/avatar";
 import { Flex, IconButton, Text } from "@chakra-ui/react";
-import { url } from "inspector";
-import { FaInstagram } from "react-icons/fa";
+import { FaFigma, FaInstagram, FaLaravel } from "react-icons/fa";
+import { FaHtml5, FaJava, FaReact } from "react-icons/fa6";
 import { FiGithub } from "react-icons/fi";
+import { IoLogoCss3, IoLogoJavascript } from "react-icons/io";
+import { RiPhpFill, RiTailwindCssFill } from "react-icons/ri";
+import { SiChakraui, SiNextdotjs, SiPostman } from "react-icons/si";
+import { TbSql } from "react-icons/tb";
 
 export default function Index() {
     return (
@@ -32,88 +36,215 @@ export default function Index() {
                 w="full"
                 h="full"
                 p="24px"
-                bg="rgba(255, 255, 255, 0.1)"
+                bg="rgba(0, 255, 255, 0.1)"
                 borderRadius="16px"
                 boxShadow="0 4px 30px rgba(31, 38, 135, 0.37)"
                 backdropFilter="blur(1px)"
                 border="1px solid rgba(255, 255, 255, 0.18)"
                 gap={"24px"}>
-                <Flex w={"full"} h={"156px"} gap={"24px"}>
-                    <Flex w={"fit-content"} h={"full"}>
-                        <Flex
-                            w="156px" // Slightly larger than the avatar for styling
-                            h="156px"
-                            justifyContent="center"
-                            alignItems="center"
-                            bg="rgba(0, 0, 0, 0.45)"
-                            borderRadius="50%" // Circular container
-                            backdropFilter="blur(5px)" // Blur effect
-                            boxShadow="0 4px 15px rgba(0, 0, 0, 0.2)" // Subtle shadow for depth
-                        >
-                            <Avatar w="140px" h="140px" src="/avatar.jpg" />
+                <Flex w={"full"} h={"full"} direction={"column"} gap={"18px"}>
+                    <Flex w={"full"} h={"156px"} gap={"24px"}>
+                        <Flex w={"fit-content"} h={"full"}>
+                            <Flex
+                                w="156px"
+                                h="156px"
+                                justifyContent="center"
+                                alignItems="center"
+                                bg="rgba(0, 0, 0, 0.45)"
+                                borderRadius="50%"
+                                backdropFilter="blur(5px)"
+                                boxShadow="0 4px 15px rgba(0, 0, 0, 0.2)">
+                                <Avatar w="140px" h="140px" src="/avatar.jpg" />
+                            </Flex>
                         </Flex>
-                    </Flex>
 
-                    <Flex
-                        w={"full"}
-                        h={"full"}
-                        direction={"column"}
-                        gap={"8px"}>
                         <Flex
                             w={"full"}
                             h={"full"}
                             direction={"column"}
-                            gap={"2px"}
-                            bg="rgba(0, 0, 0, 0.45)"
-                            backdropFilter="blur(5px)"
-                            boxShadow="0 4px 15px rgba(0, 0, 0, 0.2)"
-                            rounded={"20px"}
-                            p={"12px"}>
-                            <Text fontSize={"20px"} fontWeight={"bold"}>
-                                I Gusti Ngurah Arya Phalguna Abhimantra Putra
-                            </Text>
-                            <Text fontSize={"16px"} fontWeight={"semibold"}>
-                                Frontend Developer, UI UX Designer, Project
-                                Manager
-                            </Text>
-                            <Text fontSize={"12px"} fontWeight={"extralight"}>
-                                Denpasar, Bali
+                            gap={"8px"}>
+                            <Flex
+                                w={"full"}
+                                h={"full"}
+                                direction={"column"}
+                                gap={"2px"}
+                                bg="rgba(0, 0, 0, 0.45)"
+                                backdropFilter="blur(5px)"
+                                boxShadow="0 4px 15px rgba(0, 0, 0, 0.2)"
+                                rounded={"20px"}
+                                p={"12px"}>
+                                <Text fontSize={"20px"} fontWeight={"bold"}>
+                                    I Gusti Ngurah Arya Phalguna Abhimantra
+                                    Putra
+                                </Text>
+                                <Text fontSize={"16px"} fontWeight={"semibold"}>
+                                    Frontend Developer, UI/UX Designer, Project
+                                    Manager
+                                </Text>
+                                <Text
+                                    fontSize={"12px"}
+                                    fontWeight={"extralight"}>
+                                    Denpasar, Bali
+                                </Text>
+                            </Flex>
+                            <Flex
+                                w={"fit-content"}
+                                h={"fit-content"}
+                                gap={"12px"}
+                                px={"12px"}
+                                py={"8px"}
+                                bg="rgba(0, 0, 0, 0.45)"
+                                backdropFilter="blur(5px)"
+                                boxShadow="0 4px 15px rgba(0, 0, 0, 0.2)"
+                                rounded={"28px"}>
+                                <IconButton
+                                    size={"xs"}
+                                    rounded={"full"}
+                                    variant={"outline"}
+                                    border={"1px solid white"}>
+                                    <FiGithub></FiGithub>
+                                </IconButton>
+                                <IconButton
+                                    size={"xs"}
+                                    rounded={"full"}
+                                    variant={"outline"}
+                                    border={"1px solid white"}>
+                                    <FaInstagram></FaInstagram>
+                                </IconButton>
+                                <IconButton
+                                    size={"xs"}
+                                    rounded={"full"}
+                                    variant={"outline"}
+                                    border={"1px solid white"}>
+                                    <FiGithub></FiGithub>
+                                </IconButton>
+                            </Flex>
+                        </Flex>
+                    </Flex>
+                    <Flex
+                        w={"full"}
+                        h={"45%"}
+                        bg="rgba(0, 0, 0, 0.45)"
+                        backdropFilter="blur(5px)"
+                        boxShadow="0 4px 15px rgba(0, 0, 0, 0.2)"
+                        rounded={"20px"}
+                        p={"12px"}
+                        direction={"column"}
+                        gap={"8px"}>
+                        <Flex>
+                            <Text fontSize={"20px"} fontWeight={"semibold"}>
+                                Tech Stack
                             </Text>
                         </Flex>
-                        <Flex
-                            w={"fit-content"}
-                            h={"fit-content"}
-                            gap={"12px"}
-                            px={"12px"}
-                            py={"8px"}
-                            bg="rgba(0, 0, 0, 0.45)"
-                            backdropFilter="blur(5px)"
-                            boxShadow="0 4px 15px rgba(0, 0, 0, 0.2)"
-                            rounded={"28px"}>
+                        <Flex wrap={"wrap"} gap={"14px"}>
                             <IconButton
-                                size={"xs"}
+                                size={"xl"}
                                 rounded={"full"}
                                 variant={"outline"}
-                                border={"1px solid white"}>
-                                <FiGithub></FiGithub>
+                                border={"1px solid white"}
+                                >
+                                <FaHtml5></FaHtml5>
                             </IconButton>
                             <IconButton
-                                size={"xs"}
+                                size={"xl"}
                                 rounded={"full"}
                                 variant={"outline"}
-                                border={"1px solid white"}>
-                                <FaInstagram></FaInstagram>
+                                border={"1px solid white"}
+                                >
+                                <IoLogoCss3></IoLogoCss3>
                             </IconButton>
                             <IconButton
-                                size={"xs"}
+                                size={"xl"}
                                 rounded={"full"}
                                 variant={"outline"}
-                                border={"1px solid white"}>
-                                <FiGithub></FiGithub>
+                                border={"1px solid white"}
+                                >
+                                <IoLogoJavascript></IoLogoJavascript>
+                            </IconButton>
+                            <IconButton
+                                size={"xl"}
+                                rounded={"full"}
+                                variant={"outline"}
+                                border={"1px solid white"}
+                                >
+                                <RiTailwindCssFill></RiTailwindCssFill>
+                            </IconButton>
+                            <IconButton
+                                size={"xl"}
+                                rounded={"full"}
+                                variant={"outline"}
+                                border={"1px solid white"}
+                                >
+                                <TbSql></TbSql>
+                            </IconButton>
+                            <IconButton
+                                size={"xl"}
+                                rounded={"full"}
+                                variant={"outline"}
+                                border={"1px solid white"}
+                                >
+                                <FaJava></FaJava>
+                            </IconButton>
+                            <IconButton
+                                size={"xl"}
+                                rounded={"full"}
+                                variant={"outline"}
+                                border={"1px solid white"}
+                                >
+                                <RiPhpFill></RiPhpFill>
+                            </IconButton>
+                            <IconButton
+                                size={"xl"}
+                                rounded={"full"}
+                                variant={"outline"}
+                                border={"1px solid white"}
+                                >
+                                <FaLaravel></FaLaravel>
+                            </IconButton>
+                            <IconButton
+                                size={"xl"}
+                                rounded={"full"}
+                                variant={"outline"}
+                                border={"1px solid white"}
+                                >
+                                <FaReact></FaReact>
+                            </IconButton>
+                            <IconButton
+                                size={"xl"}
+                                rounded={"full"}
+                                variant={"outline"}
+                                border={"1px solid white"}
+                                >
+                                <SiNextdotjs></SiNextdotjs>
+                            </IconButton>
+                            <IconButton
+                                size={"xl"}
+                                rounded={"full"}
+                                variant={"outline"}
+                                border={"1px solid white"}
+                                >
+                                <SiChakraui></SiChakraui>
+                            </IconButton>
+                            <IconButton
+                                size={"xl"}
+                                rounded={"full"}
+                                variant={"outline"}
+                                border={"1px solid white"}
+                                >
+                                <FaFigma></FaFigma>
+                            </IconButton>
+                            <IconButton
+                                size={"xl"}
+                                rounded={"full"}
+                                variant={"outline"}
+                                border={"1px solid white"}
+                                >
+                                <SiPostman></SiPostman>
                             </IconButton>
                         </Flex>
                     </Flex>
                 </Flex>
+
                 <Flex
                     w={"70%"}
                     h={"full"}
